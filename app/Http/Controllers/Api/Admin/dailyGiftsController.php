6dac6cd9591d1834310f6d6bc7eab1ddd9c936ac
shehab <sh.hosni@glitchfix.net>
2021-06-07 14:01:25 +0200

    crud for daily gift

---
+++ app/Http/Controllers/Api/Admin/dailyGiftsController.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\daily_gift;
+use Validator;
+use DB;
+
+class dailyGiftsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $daily_gift = daily_gift::paginate(25);
+        // return
+        return $this->successResponse($daily_gift,'response success');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules =  [
+            'name'    => 'required',
+            ];
+            $validator = Validator::make(request()->all(), $rules);
+            if($validator->fails()) {
+                return $this->errorResponse($validator->errors()->all()[0]);
+            }
+            // check on gift 
+            if($request->gift_id){
+                $gifts = DB::table('gifts')->where('id',$request->gift_id)->first();
+                if(empty($gifts)){
+                    return $this->errorResponse('this gift Not Found');
+                    // if(empty($gifts) && !empty(daily_gift::where('gift_id',$request->gift_id)->first()))
+                    // {
+                    //     return $this->errorResponse('this gift already taken');
+                    // }
+                }
+            }
+            // check on item 
+            if($request->item_id){
+                $item = DB::table('items')->where('id',$request->item_id)->first();
+                if(empty($item)){
+                    return $this->errorResponse('this item Not Found');
+                    // if(daily_gift::where('item_id',$request->item_id)->first())
+                    // {
+                    //     return $this->errorResponse('this item already taken');
+                    // }
+                }
+            }
+
+            // begin store
+            if (daily_gift::get()->count() > 7) {
+                return $this->errorResponse('cant add more then 7 gifts');
+            }
+            DB::beginTransaction();
+            $data = [];
+            try{
+                $data['name'] = $request->name;
+                if($request->image_link)
+                {
+                    $data['image_link'] = $this->uploadBase64($request->image_link);
+                }
+                if ($request->gift_id) {
+                    $data['gift_id'] = $request->gift_id;
+                }
+                if ($request->item_id) {
+                    $data['item_id'] = $request->item_id;
+                }
+                if ($request->coins) {
+                    $data['coins'] = $request->coins;
+                }
+                $stored = DB::table('daily_gift')->insert($data);
+                DB::commit();
+                return $this->successResponse($data, 'success added');
+                // data stored
+            } catch (\Exception $e) {
+                DB::rollback();
+                return $this->formatErrors($e,'fatel error');
+                // something went wrong
+            }
+        }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $dailyObj = daily_gift::where('id',$id)->first();
+        if(!$dailyObj)
+        {
+            return $this->errorResponse('daily gift not found');
+        }
+            // check on gift 
+            if($request->gift_id){
+                $gifts = DB::table('gifts')->where('id',$request->gift_id)->first();
+                if(empty($gifts)){
+                    return $this->errorResponse('this gift Not Found');
+                    // if(empty($gifts) && !empty(daily_gift::where('gift_id',$request->gift_id)->first()))
+                    // {
+                    //     return $this->errorResponse('this gift already taken');
+                    // }
+                }
+            }
+            // check on item 
+            if($request->item_id){
+                $item = DB::table('items')->where('id',$request->item_id)->first();
+                if(empty($item)){
+                    return $this->errorResponse('this item Not Found');
+                    // if(daily_gift::where('item_id',$request->item_id)->first())
+                    // {
+                    //     return $this->errorResponse('this item already taken');
+                    // }
+                }
+            }
+
+            // begin store
+            DB::beginTransaction();
+            $data = [];
+            try{
+                if($request->name)
+                {
+                    $data['name'] = $request->name;
+                }
+                if($request->image_link)
+                {
+                    $data['image_link'] = $this->uploadBase64($request->image_link);
+                }
+                if ($request->gift_id) {
+                    $data['gift_id'] = $request->gift_id;
+                    $data['item_id'] = null;
+                    $data['coins'] = null;
+                }
+                if ($request->item_id) {
+                    $data['item_id'] = $request->item_id;
+                    $data['gift_id'] = null;
+                    $data['coins'] = null;
+                }
+                if ($request->coins) {
+                    $data['coins'] = $request->coins;
+                    $data['gift_id'] = null;
+                    $data['item_id'] = null;
+                }
+                $stored = DB::table('daily_gift')->where('id',$id)->update($data);
+                DB::commit();
+                return $this->successResponse($data, 'success added');
+                // data stored
+            } catch (\Exception $e) {
+                DB::rollback();
+                return $this->formatErrors($e,'fatel error');
+                // something went wrong
+            }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if(!daily_gift::where('id',$id)->first()){
+            return $this->errorResponse("daily gift not found");
+        }
+        DB::beginTransaction();
+        $data = [];
+        try{
+            daily_gift::where('id',$id)->delete();
+            DB::commit();
+            return $this->successResponse($data, 'success deleted');
+            // data stored
+        } catch (\Exception $e) {
+            DB::rollback();
+            return $this->formatErrors($e,'fatel error');
+            // something went wrong
+        }
+    }
+}
