be7ae53b382f330c5420fa67fe35073a9bfe63d0
m.beshbeshy <m.beshbeshy@glitchffix.net>
2021-06-08 18:21:38 +0200

    vip tiers table and privilages set-up

--- app/Http/Controllers/Api/Rooms/RoomController.php
+++ app/Http/Controllers/Api/Rooms/RoomController.php
@@ -4,20 +4,40 @@ namespace App\Http\Controllers\Api\Rooms;
 
 use App\Http\Controllers\Controller;
 use App\Models\Room;
+use App\Models\Vip_tiers;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class RoomController extends Controller
 {
-    //
 
-//    public function create_room_password(Request $request){
-//        return $this->successResponse($request->user(), 'authorized');
-//        if ($request->user()->cannot('room_password', Room::class)) {
-//            abort(403);
-//        }
-//        else{
-//            return $this->successResponse(null, 'authorized');
-//        }
+
+    public function create_room_password(Request $request){
+
+        if ($this->user()->can('room_password', Room::class)) {
+            return $this->successResponse(null, 'authorized');
+        }
+        else{
+            return $this->errorResponse('un-authorized');
+        }
+    }
+
+//    public function store(Request $request)
+//    {
+//        dd(request('name'));
+//        $request->validate([
+//            'privileges.*' => 'required',
+//            'renew_price' => 'required',
+//            'name' => 'required',
+//            'price' => 'required'
+//        ]);
+//
+//        $tier = new Vip_tiers;
+//        $tier->privileges = json_encode($request->privileges);
+//        $tier->renew_price = $request->renew_price;
+//        $tier->name = $request->name;
+//        $tier->price = $request->price;
+//        $tier->save();
+//        dd($tier);
+//
 //    }
 }
