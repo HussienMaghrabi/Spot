99b1a37262bf46de13dee1b54e6dc654f9697ad8
HusseinMaghrabi <h.el-maghraby@glitchfix.net>
2021-07-06 13:03:23 +0200

    flag for country

--- database/seeders/CountriesSeeder.php
+++ database/seeders/CountriesSeeder.php
@@ -17,12 +17,15 @@ class CountriesSeeder extends Seeder
     {
         DB::table('contries')->insert([
             'name' => 'UAE',
+            'flag' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_United_Arab_Emirates.svg/1280px-Flag_of_the_United_Arab_Emirates.svg.png',
         ]);
         DB::table('contries')->insert([
             'name' => 'EGY',
+            'flag' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/255px-Flag_of_Egypt.svg.png',
         ]);
         DB::table('contries')->insert([
             'name' => 'KSA',
+            'flag' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/2560px-Flag_of_Saudi_Arabia.svg.png',
         ]);
     }
 }
