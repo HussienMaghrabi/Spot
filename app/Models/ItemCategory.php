d4359cb75e39a717f33faec9a98a45849ee7746f
HussienMaghrabi <elmaghrabihussien@gmail.com>
2021-07-15 18:32:43 +0200

    user_item

--- app/Models/ItemCategory.php
+++ app/Models/ItemCategory.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class ItemCategory extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function getImageAttribute($value)
+    {
+        if ($value)
+        {
+            return asset(Storage::url($value));
+        }
+    }
 }
