3c2ea3d8ad7ed0a5366a319dd3b14b7522805365
m.beshbeshy <m.beshbeshy@glitchffix.net>
2021-07-27 16:33:06 +0200

    some bugs

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -123,4 +123,5 @@ return[
     'userBadgeCatLimit' => "You can't activate two badges from the same achievement.",
     'noBadges' => "You didn't select any badges to wear.",
     'allChargeLevels' => 'all Charge Levels',
+    'activity_limit' => "you can't create more than 3 per day",
 ];
