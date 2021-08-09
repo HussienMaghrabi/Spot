dee2e1f82e892236ecdd785cb5c57df93d1a1f0e
BeshBeshy <mahmoud_beshbeshy1996@hotmail.com>
2021-08-09 17:38:16 +0200

    badges and charging levels

--- database/seeders/BadgeSeeder.php
+++ database/seeders/BadgeSeeder.php
@@ -16,115 +16,461 @@ class BadgeSeeder extends Seeder
     {
         //
         DB::table('badges')->insert([
-            'name' => 'flower',
+            'name' => 'الغرفة الثرية',
             'img_link' => 'uploads/badges/الغرفة_الثرية_برونزي.png',
             'gift_id' => '1',
-            'badgeCategory_id' => '2',
+            'badgeCategory_id' => '3',
             'category_id' => '1',
             'amount' => '10',
         ]);
         DB::table('badges')->insert([
-            'name' => 'flower',
+            'name' => 'الغرفة الثرية',
             'img_link' => 'uploads/badges/الغرفة_الثرية_فضي.png',
             'gift_id' => '1',
-            'badgeCategory_id' => '2',
+            'badgeCategory_id' => '3',
             'category_id' => '1',
             'amount' => '20',
         ]);
         DB::table('badges')->insert([
-            'name' => 'flower',
+            'name' => 'الغرفة الثرية',
             'img_link' => 'uploads/badges/الغرفة_الثرية_ذهبي.png',
             'gift_id' => '1',
-            'badgeCategory_id' => '2',
+            'badgeCategory_id' => '3',
             'category_id' => '1',
             'amount' => '30',
         ]);
         DB::table('badges')->insert([
-            'name' => 'kiss',
+            'name' => 'الغرفة الساخنة',
             'img_link' => 'uploads/badges/الغرفة_الساخنة_برونزي.png',
             'gift_id' => '2',
-            'badgeCategory_id' => '2',
+            'badgeCategory_id' => '3',
             'category_id' => '2',
             'amount' => '10',
         ]);
         DB::table('badges')->insert([
-            'name' => 'kiss',
+            'name' => 'الغرفة الساخنة',
             'img_link' => 'uploads/badges/الغرفة_الساخنة_الفضية.png',
             'gift_id' => '2',
-            'badgeCategory_id' => '2',
+            'badgeCategory_id' => '3',
             'category_id' => '2',
             'amount' => '20',
         ]);
         DB::table('badges')->insert([
-            'name' => 'kiss',
+            'name' => 'الغرفة الساخنة',
             'img_link' => 'uploads/badges/الغرفة_الساخنة_ذهبي.png',
             'gift_id' => '2',
-            'badgeCategory_id' => '2',
+            'badgeCategory_id' => '3',
             'category_id' => '2',
             'amount' => '30',
         ]);
         DB::table('badges')->insert([
-            'name' => 'friends',
+            'name' => 'النجم النشط',
             'img_link' => 'uploads/badges/النجم_النشط_برونزي.png',
             'amount' => '10',
             'badgeCategory_id' => '3',
             'category_id' => '3',
         ]);
         DB::table('badges')->insert([
-            'name' => 'friends',
+            'name' => 'النجم النشط',
             'img_link' => 'uploads/badges/النجم_النشط_الفضي.png',
             'amount' => '20',
             'badgeCategory_id' => '3',
             'category_id' => '3',
         ]);
         DB::table('badges')->insert([
-            'name' => 'friends',
+            'name' => 'النجم النشط',
             'img_link' => 'uploads/badges/النجم_النشط_ذهبي.png',
             'amount' => '30',
             'badgeCategory_id' => '3',
             'category_id' => '3',
         ]);
         DB::table('badges')->insert([
-            'name' => 'login',
+            'name' => 'مجنون التسوق',
             'img_link' => 'uploads/badges/مجنون_التسوق_برونزي.png',
             'amount' => '10',
-            'badgeCategory_id' => '1',
+            'badgeCategory_id' => '3',
             'category_id' => '4',
         ]);
         DB::table('badges')->insert([
-            'name' => 'login',
+            'name' => 'مجنون التسوق',
             'img_link' => 'uploads/badges/مجنون_التسوق_فضي.png',
             'amount' => '20',
-            'badgeCategory_id' => '1',
+            'badgeCategory_id' => '3',
             'category_id' => '4',
         ]);
         DB::table('badges')->insert([
-            'name' => 'login',
+            'name' => 'مجنون التسوق',
             'img_link' => 'uploads/badges/مجنون_التسوق_ذهبي.png',
             'amount' => '30',
-            'badgeCategory_id' => '1',
+            'badgeCategory_id' => '3',
             'category_id' => '4',
         ]);
         DB::table('badges')->insert([
-            'name' => 'level',
+            'name' => 'نجم حقيبة الحظ',
             'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_برونزي.png',
             'amount' => '10',
             'badgeCategory_id' => '3',
             'category_id' => '5',
         ]);
         DB::table('badges')->insert([
-            'name' => 'level',
+            'name' => 'نجم حقيبة الحظ',
             'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_فضي.png',
             'amount' => '20',
             'badgeCategory_id' => '3',
             'category_id' => '5',
         ]);
         DB::table('badges')->insert([
-            'name' => 'level',
+            'name' => 'نجم حقيبة الحظ',
             'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_ذهبي.png',
             'amount' => '30',
             'badgeCategory_id' => '3',
             'category_id' => '5',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم محبوب',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_برونزي.png',
+            'amount' => '10',
+            'badgeCategory_id' => '3',
+            'category_id' => '9',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم محبوب',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_فضي.png',
+            'amount' => '20',
+            'badgeCategory_id' => '3',
+            'category_id' => '9',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم محبوب',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_ذهبي.png',
+            'amount' => '30',
+            'badgeCategory_id' => '3',
+            'category_id' => '9',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'مليونير',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_برونزي.png',
+            'amount' => '10',
+            'badgeCategory_id' => '3',
+            'category_id' => '6',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'مليونير',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_فضي.png',
+            'amount' => '20',
+            'badgeCategory_id' => '3',
+            'category_id' => '6',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'مليونير',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_ذهبي.png',
+            'amount' => '30',
+            'badgeCategory_id' => '3',
+            'category_id' => '6',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نشط اجتماعي',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_برونزي.png',
+            'amount' => '10',
+            'badgeCategory_id' => '3',
+            'category_id' => '10',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نشط اجتماعي',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_فضي.png',
+            'amount' => '20',
+            'badgeCategory_id' => '3',
+            'category_id' => '10',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نشط اجتماعي',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_ذهبي.png',
+            'amount' => '30',
+            'badgeCategory_id' => '3',
+            'category_id' => '10',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم لامع',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_برونزي.png',
+            'amount' => '10',
+            'badgeCategory_id' => '3',
+            'category_id' => '8',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم لامع',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_فضي.png',
+            'amount' => '20',
+            'badgeCategory_id' => '3',
+            'category_id' => '8',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم لامع',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_ذهبي.png',
+            'amount' => '30',
+            'badgeCategory_id' => '3',
+            'category_id' => '8',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم تسجيل الدخول',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_برونزي.png',
+            'amount' => '10',
+            'badgeCategory_id' => '3',
+            'category_id' => '7',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم تسجيل الدخول',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_فضي.png',
+            'amount' => '20',
+            'badgeCategory_id' => '3',
+            'category_id' => '7',
+        ]);
+        DB::table('badges')->insert([
+            'name' => 'نجم تسجيل الدخول',
+            'img_link' => 'uploads/badges/نجم_حقيبة_الحظ_ذهبي.png',
+            'amount' => '30',
+            'badgeCategory_id' => '3',
+            'category_id' => '7',
+        ]);
+
+        DB::table('badges')->insert([
+            'name' => '50k',
+            'img_link' => 'uploads/badges/50k.png',
+            'amount' => '50000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '100k',
+            'img_link' => 'uploads/badges/100k.png',
+            'amount' => '100000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '200k',
+            'img_link' => 'uploads/badges/200k.png',
+            'amount' => '200000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '300k',
+            'img_link' => 'uploads/badges/300k.png',
+            'amount' => '300000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '400k',
+            'img_link' => 'uploads/badges/400k.png',
+            'amount' => '400000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '500k',
+            'img_link' => 'uploads/badges/500k.png',
+            'amount' => '500000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '600k',
+            'img_link' => 'uploads/badges/600k.png',
+            'amount' => '600000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '700k',
+            'img_link' => 'uploads/badges/700k.png',
+            'amount' => '700000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '800k',
+            'img_link' => 'uploads/badges/800k.png',
+            'amount' => '800000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '900k',
+            'img_link' => 'uploads/badges/900k.png',
+            'amount' => '900000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '1M',
+            'img_link' => 'uploads/badges/1M.png',
+            'amount' => '1000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '2M',
+            'img_link' => 'uploads/badges/2M.png',
+            'amount' => '2000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '3M',
+            'img_link' => 'uploads/badges/3M.png',
+            'amount' => '3000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '4M',
+            'img_link' => 'uploads/badges/4M.png',
+            'amount' => '4000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '5M',
+            'img_link' => 'uploads/badges/5M.png',
+            'amount' => '5000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '7M',
+            'img_link' => 'uploads/badges/7M.png',
+            'amount' => '7000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '8M',
+            'img_link' => 'uploads/badges/8M.png',
+            'amount' => '8000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '10M',
+            'img_link' => 'uploads/badges/10M.png',
+            'amount' => '10000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '12M',
+            'img_link' => 'uploads/badges/12M.png',
+            'amount' => '12000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '15M',
+            'img_link' => 'uploads/badges/15M.png',
+            'amount' => '15000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '18M',
+            'img_link' => 'uploads/badges/18M.png',
+            'amount' => '18000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '20M',
+            'img_link' => 'uploads/badges/20M.png',
+            'amount' => '20000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '22M',
+            'img_link' => 'uploads/badges/22M.png',
+            'amount' => '22000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '25M',
+            'img_link' => 'uploads/badges/25M.png',
+            'amount' => '25000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '30M',
+            'img_link' => 'uploads/badges/30M.png',
+            'amount' => '30000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '33M',
+            'img_link' => 'uploads/badges/33M.png',
+            'amount' => '33000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '35M',
+            'img_link' => 'uploads/badges/35M.png',
+            'amount' => '35000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '40M',
+            'img_link' => 'uploads/badges/40M.png',
+            'amount' => '40000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '45M',
+            'img_link' => 'uploads/badges/45M.png',
+            'amount' => '45000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
+        ]);
+        DB::table('badges')->insert([
+            'name' => '50M',
+            'img_link' => 'uploads/badges/50M.png',
+            'amount' => '50000000',
+            'badgeCategory_id' => '3',
+            'category_id' => '11',
+            'charge_badge' => '1',
         ]);
     }
 }
--- database/seeders/chargingLevelSeeder.php
+++ database/seeders/chargingLevelSeeder.php
@@ -18,7 +18,7 @@ class chargingLevelSeeder extends Seeder
             'name' => '50 K',
             'level_limit' => 50000,
             'desc' => 'description',
-            'badge_id' => '1',
+            'badge_id' => '31',
             'levelNo' => '1',
         ]);
         DB::table('charging_levels')->insert([
@@ -26,7 +26,7 @@ class chargingLevelSeeder extends Seeder
             'level_limit' => 100000,
             'desc' => 'description',
             'gift_id' => '["1","2"]',
-            'badge_id' => '2',
+            'badge_id' => '32',
             'levelNo' => '2',
         ]);
         DB::table('charging_levels')->insert([
@@ -34,8 +34,224 @@ class chargingLevelSeeder extends Seeder
             'level_limit' => 200000,
             'desc' => 'description',
             'gift_id' => '["1","2"]',
-            'badge_id' => '3',
+            'badge_id' => '33',
             'levelNo' => '3',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '300 K',
+            'level_limit' => 300000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '34',
+            'levelNo' => '4',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '400 K',
+            'level_limit' => 400000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '35',
+            'levelNo' => '5',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '500 K',
+            'level_limit' => 500000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '36',
+            'levelNo' => '6',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '600 K',
+            'level_limit' => 600000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '37',
+            'levelNo' => '7',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '700 K',
+            'level_limit' => 700000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '38',
+            'levelNo' => '8',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '800 K',
+            'level_limit' => 800000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '39',
+            'levelNo' => '9',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '900 K',
+            'level_limit' => 900000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '40',
+            'levelNo' => '10',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '1 M',
+            'level_limit' => 1000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '41',
+            'levelNo' => '11',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '2 M',
+            'level_limit' => 2000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '42',
+            'levelNo' => '12',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '3 M',
+            'level_limit' => 3000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '43',
+            'levelNo' => '13',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '4 M',
+            'level_limit' => 4000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '44',
+            'levelNo' => '14',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '5 M',
+            'level_limit' => 5000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '45',
+            'levelNo' => '15',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '7 M',
+            'level_limit' => 7000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '46',
+            'levelNo' => '16',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '8 M',
+            'level_limit' => 8000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '47',
+            'levelNo' => '17',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '10 M',
+            'level_limit' => 10000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '48',
+            'levelNo' => '18',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '12 M',
+            'level_limit' => 12000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '49',
+            'levelNo' => '19',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '15 M',
+            'level_limit' => 15000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '50',
+            'levelNo' => '20',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '18 M',
+            'level_limit' => 18000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '51',
+            'levelNo' => '21',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '20 M',
+            'level_limit' => 20000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '52',
+            'levelNo' => '22',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '22 M',
+            'level_limit' => 22000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '53',
+            'levelNo' => '23',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '25 M',
+            'level_limit' => 25000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '54',
+            'levelNo' => '24',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '30 M',
+            'level_limit' => 30000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '55',
+            'levelNo' => '25',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '33 M',
+            'level_limit' => 33000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '56',
+            'levelNo' => '26',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '35 M',
+            'level_limit' => 35000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '57',
+            'levelNo' => '27',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '40 M',
+            'level_limit' => 40000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '58',
+            'levelNo' => '28',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '45 M',
+            'level_limit' => 45000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '59',
+            'levelNo' => '29',
+        ]);
+        DB::table('charging_levels')->insert([
+            'name' => '50 M',
+            'level_limit' => 50000000,
+            'desc' => 'description',
+            'gift_id' => '["1","2"]',
+            'badge_id' => '60',
+            'levelNo' => '30',
         ]);
     }
 }
