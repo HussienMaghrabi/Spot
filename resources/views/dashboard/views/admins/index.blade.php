acf4aa0eb6c67a3911cf5520aef3eaa6832c4cca
HusseinMaghrabi <h.el-maghraby@glitchfix.net>
2021-08-10 18:33:23 +0200

    fix bugs

--- resources/views/dashboard/views/admins/index.blade.php
+++ resources/views/dashboard/views/admins/index.blade.php
@@ -10,73 +10,65 @@
 @extends('dashboard.layouts.app')
 @section('title', __('dashboard.'.$resource['title']))
 @section('content')
-    @include('dashboard.components.header')
+        <div class="card card-custom">
 
-        <div class="d-flex flex-column-fluid">
-            <div class="container">
-                <div class="card card-custom">
-
-                    <div class="card-header flex-wrap border-0 pt-6 pb-0">
-                        <div class="card-title">
-                            <h3 class="card-label"><i class="fa fa-fw fa-{{$resource['icon']}}"></i>{{ __('dashboard.'.$resource['title'])}}</h3>
-                        </div>
-                        <div class="card-toolbar">
-                            <form class="input-group input-group-sm"  action="{{route($resource['route'].'.search', ['lang' => App::getLocale()])}}" method="post">
-                                @csrf
-                                <input type="text" name="text" class="form-control pull-right" placeholder="{{__('dashboard.Search')}}" style="height: 35px;width: 150px;">
-                                <button type="submit" class="btn btn-default" title="Search"><i class="fa fa-search"></i></button>
-                                <a  href="{{route($resource['route'].'.create', ['lang' => App::getLocale()])}}" class="btn btn-default" title="New Item"><i class="fa fa-plus"></i></a>
+            <div class="card-header flex-wrap border-0 pt-6 pb-0">
+                <div class="card-title">
+                    <h3 class="card-label"><i class="fa fa-fw fa-{{$resource['icon']}}"></i>{{ __('dashboard.'.$resource['title'])}}</h3>
+                </div>
+                <div class="card-toolbar">
+                    <form class="input-group input-group-sm"  action="{{route($resource['route'].'.search', ['lang' => App::getLocale()])}}" method="post">
+                        @csrf
+                        <input type="text" name="text" class="form-control pull-right" placeholder="{{__('dashboard.Search')}}" style="height: 35px;width: 150px;">
+                        <button type="submit" class="btn btn-default" title="Search"><i class="fa fa-search"></i></button>
+                        <a  href="{{route($resource['route'].'.create', ['lang' => App::getLocale()])}}" class="btn btn-default" title="New Item"><i class="fa fa-plus"></i></a>
 {{--                                <a href="#" class="btn btn-default delete_all disabled" data-toggle="modal" data-target="#danger_all" title="Delete"><i class="fa fa-fw fa-trash text-red"></i></a>--}}
-                            </form>
+                    </form>
 {{--                            @include('dashboard.components.dangerModalMulti')--}}
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        {!! Form::open(['method'=>'DELETE', 'route'=> [$resource['route'].'.multiDelete', App::getLocale()], 'class'=>'delete-form'])!!}
-                            @if(count($data) == 0)
-                                <div class="col-xs-12">
-                                    <h4> {{ __('dashboard.No Data') }}</h4>
-                                    <p>{{ __('dashboard.Add Link') }}  <b><a href="{{route($resource['route'].'.create', App::getLocale())}}">{{ __('dashboard.here') }}</a></b>.</p>
-                                </div>
-                            @else
-                                <table class="table table-separate table-head-custom table-checkable" id="kt_datatable_2">
-                                    <thead>
-                                    <tr>
-                                        @foreach($tableCols as $col)
-                                            <td><strong>{{ $col }}</strong></td>
-                                        @endforeach
-                                        <td><strong>{{__('dashboard.Actions')}}</strong></td>
-{{--                                        <td><strong><input type="checkbox" id="master"></strong></td>--}}
-
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($data as $item)
-                                    <tr class="tr-{{ $item->id }}">
-                                        <td>{{ $item->name }}</td>
-                                        <td>{{ $item->email }}</td>
-                                        <td>
-                                            <a href="{{ route($resource['route'].'.edit', [App::getLocale(), $item->id]) }}" title="edit"><i class="fa fa-fw fa-edit text-yellow"></i></a>
-                                            <a href="#" data-toggle="modal" data-target="#modal1" title="Delete"><i class="fa fa-fw fa-trash text-red"></i></a>
-                                        </td>
-{{--                                        <td><input type="checkbox" class="sub_chk" name="checked[]" value="{{$item->id}}"></td>--}}
-
-                                    </tr>
-                                    @include('dashboard.components.dangerModal', ['user_name' => $item->name, 'id' => $item->id, 'resource' => $resource['route']])
-                                    @endforeach
-
-                                    </tbody>
-                                </table>
-                        @endif
-                        {!! Form::close()!!}
-                        <div class="text-center" >
-                            {{ $data->links()}}
-                        </div>
-
-                    </div>
                 </div>
             </div>
+            <div class="card-body">
+                {!! Form::open(['method'=>'DELETE', 'route'=> [$resource['route'].'.multiDelete', App::getLocale()], 'class'=>'delete-form'])!!}
+                    @if(count($data) == 0)
+                        <div class="col-xs-12">
+                            <h4> {{ __('dashboard.No Data') }}</h4>
+                            <p>{{ __('dashboard.Add Link') }}  <b><a href="{{route($resource['route'].'.create', App::getLocale())}}">{{ __('dashboard.here') }}</a></b>.</p>
+                        </div>
+                    @else
+                        <table class="table table-separate table-head-custom table-checkable" id="kt_datatable_2">
+                            <thead>
+                            <tr>
+                                @foreach($tableCols as $col)
+                                    <td><strong>{{ $col }}</strong></td>
+                                @endforeach
+                                <td><strong>{{__('dashboard.Actions')}}</strong></td>
+{{--                                        <td><strong><input type="checkbox" id="master"></strong></td>--}}
 
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($data as $item)
+                            <tr class="tr-{{ $item->id }}">
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->email }}</td>
+                                <td>
+                                    <a href="{{ route($resource['route'].'.edit', [App::getLocale(), $item->id]) }}" title="edit"><i class="fa fa-fw fa-edit text-yellow"></i></a>
+                                    <a href="#" data-toggle="modal" data-target="#modal1" title="Delete"><i class="fa fa-fw fa-trash text-red"></i></a>
+                                </td>
+{{--                                        <td><input type="checkbox" class="sub_chk" name="checked[]" value="{{$item->id}}"></td>--}}
+
+                            </tr>
+                            @include('dashboard.components.dangerModal', ['user_name' => $item->name, 'id' => $item->id, 'resource' => $resource['route']])
+                            @endforeach
+
+                            </tbody>
+                        </table>
+                @endif
+                {!! Form::close()!!}
+
+
+            </div>
         </div>
+
 
 @endsection
