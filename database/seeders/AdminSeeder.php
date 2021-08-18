f472b8456be11d7d808d8d82710e7011269d9d17
HussienMaghrabi <elmaghrabihussien@gmail.com>
2021-08-18 16:03:37 +0200

    room active user

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,13 +15,19 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
         DB::table('admins')->insert([
             'name' => 'Admin1',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('password'),
             'profile_pic' => 'uploads/users/tux0RCdSoCQ4ALHuVAarpGtxUu7TGlUCVrG2MKLG.png',
             'api_token' => 'ZdP0jEjZ2BuipVoIxrQKmKGqEg9wJkEHBKFi2qGIZ4bdOkmaE2Cdx14kybBLGt34ITnu6Q'
+        ]);
+
+        DB::table('admins')->insert([
+            'name' => 'Hussein El-Maghrabi',
+            'email' => 'admin@admin.com',
+            'super' => '1',
+            'password' => Hash::make('123456'),
         ]);
     }
 }
