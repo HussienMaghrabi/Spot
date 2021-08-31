86ebaffb7cebdac1175b973d868ea56a56062b10
BeshBeshy <mahmoud_beshbeshy1996@hotmail.com>
2021-08-31 17:55:17 +0200

    charging exp

--- app/Http/Controllers/Api/levels/DailyExpController.php
+++ app/Http/Controllers/Api/levels/DailyExpController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\DailyLimitExp;
 use App\Models\UserDailyLimitExp;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 // return the added exp according to the current exp for the user and max limit he can get
 class DailyExpController extends Controller
@@ -128,8 +127,8 @@ class DailyExpController extends Controller
         }
     }
     // takes number of gifts received and returns the added exp for the user
-    public function checkReceiveGiftExp($count){
-        $auth = $this->auth();
+    public function checkReceiveGiftExp($count, $user_id){
+        $auth = $user_id;
         $user_limit = UserDailyLimitExp::where('user_id', $auth)->select('gift_receive', 'last_day')->first();
         if( $user_limit ===  null){
             UserDailyLimitExp::create(['user_id' => $auth ]);
@@ -165,13 +164,34 @@ class DailyExpController extends Controller
             return 0;
         }
     }
+    // takes charging amount and returns added exp for the user
+    public function checkChargeExp($amount){
+        $auth = $this->auth();
+        $coins = $amount;
+        $user_limit = UserDailyLimitExp::where('user_id', $auth)->select('charge', 'last_day')->first();
+        if( $user_limit ===  null){
+            UserDailyLimitExp::create(['user_id' => $auth]);
+            $user_limit = UserDailyLimitExp::where('user_id', $auth)->select('charge', 'updated_at')->first();
+        }
+        $max_exp = DailyLimitExp::all();
+        $added_exp = $coins * $max_exp[0]->charge_val;
+        $now = Carbon::now()->format('Y-m-d');
+        // last exp was yesterday -> reset exp
+        if( $now > $user_limit->last_day ){
+            $final_exp = $added_exp;
+            UserDailyLimitExp::where('user_id', $auth)->update(['mic_exp'=>$final_exp, 'last_day'=>$now]);
+            return $added_exp;
+        }
+        // last exp is today -> add exp
+        elseif($now == $user_limit->last_day){
+            $final_exp = $user_limit->mic_exp + $added_exp;
+            UserDailyLimitExp::where('user_id', $auth)->update(['mic_exp'=>$final_exp, 'last_day'=>$now]);
+            return $added_exp;
+        }
+    }
 
     // returns added exp for the user for daily login-in
     public function checkDailyLoginExp(){
-
-    }
-    // takes charging amount and returns added exp for the user
-    public function checkChargeExp(){
 
     }
     // takes amount of coins for sent gifts and returns the added exp for the user
